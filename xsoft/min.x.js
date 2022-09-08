dc1743a273612af777001808187f96fb52eff25a
k1zn <46452865+k1zn@users.noreply.github.com>
2022-09-09 00:31:22 +0500

    Update min.x.js

--- xsoft/min.x.js
+++ xsoft/min.x.js
@@ -116,7 +116,7 @@
                     document['getElementById']('modeF')['classList']['remove']('btnSelected')
                 };
                 typeChat('Включен ручной режим ввода капчи, для открытия капчи нажмите N,F4 или же напишите в чат /buybiz (открыть чат на Т англ)');
-                typeChat('Внимание! Официальными доменами сайта являются: kiznold.github.io, k1zn.rinki.cc')
+                typeChat('Внимание! Официальными доменом сайта является ТОЛЬКО: kiznold.github.io. Совсем скоро k1zn.rinki.cc перестанет работать.')
                 mode = 0;
                 document['getElementById']('houseSale')['style']['display'] = 'none'
             };
